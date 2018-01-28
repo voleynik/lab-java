83331757444d7db21862774644f58ac8f1d33e40
vo <Kiev1kiev@>
2018-01-28 13:17:07 +0200

    Play with lambda. Added Hello classes.

---
+++ src/lab/lambda/Hello.java
@@ -0,0 +1,5 @@
+package lab.lambda;
+
+public interface Hello {
+    public void hello(String name);
+}
---
+++ src/lab/lambda/HelloRun.java
@@ -0,0 +1,25 @@
+package lab.lambda;
+
+public class HelloRun {
+
+    public static void main(String[] args) {
+
+        ////////////////// A - Traditional
+        Hello h1 = new HelloImpl();
+        h1.hello("Bill");
+
+        ////////////////// B - Anonimous class. 5 lines. No Impl needed
+        Hello h2 = new Hello() {
+            public void hello(String name) {
+                System.out.println("Hello " + name);
+            }
+        };
+        h2.hello("Steve");
+
+        ////////////////// C - Lambda 3 lines. No Impl needed
+        Hello h3 = (name) -> {
+            System.out.println("Hello " + name);
+        };
+        h3.hello("Vlad");
+    }
+}
