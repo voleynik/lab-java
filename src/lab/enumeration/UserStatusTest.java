f055bd19f61a807d2d1dec9b01ea1787b99e18aa
voleynik <vladimir.oleynik@mail.com>
2018-04-06 15:53:04 +0300

    Added prints ordinal

--- src/lab/enumeration/UserStatusTest.java
+++ src/lab/enumeration/UserStatusTest.java
@@ -1,11 +1,25 @@
 package lab.enumeration;
 
 public class UserStatusTest {
+
+    public enum UsrSts {PENDING, ACTIVE, INACTIVE, DELETED}
+
     public static void main(String[] args) {
-        System.out.println(UserStatus.PENDING);
-        System.out.println(UserStatus.ACTIVE);
-        System.out.println(UserStatus.INACTIVE);
-        System.out.println(UserStatus.DELETED);
+        System.out.println(UsrSts.PENDING);             // prints PENDING
+        System.out.println(UsrSts.PENDING.name());      // prints PENDING
+        System.out.println(UsrSts.PENDING.ordinal());   // prints 0
+        System.out.println();
+        System.out.println(UsrSts.ACTIVE);              // prints ACTIVE
+        System.out.println(UsrSts.ACTIVE.name());       // prints ACTIVE
+        System.out.println(UsrSts.ACTIVE.ordinal());    // prints 1
+        System.out.println();
+        System.out.println(UsrSts.INACTIVE);            // prints INACTIVE
+        System.out.println(UsrSts.INACTIVE.name());     // prints INACTIVE
+        System.out.println(UsrSts.INACTIVE.ordinal());  // prints 2
+        System.out.println();
+        System.out.println(UsrSts.DELETED);             // prints DELETED
+        System.out.println(UsrSts.DELETED.name());      // prints DELETED
+        System.out.println(UsrSts.DELETED.ordinal());   // prints 3
     }
 }
 /* Prints:
