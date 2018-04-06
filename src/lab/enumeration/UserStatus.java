227fca35ced44373f3569cf6657423bcba464e14
voleynik <vladimir.oleynik@mail.com>
2018-04-06 12:08:01 +0300

    Enum test

---
+++ src/lab/enumeration/UserStatus.java
@@ -0,0 +1,8 @@
+package lab.enumeration;
+
+public enum UserStatus {
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    DELETED;
+}
---
+++ src/lab/enumeration/UserStatusTest.java
@@ -0,0 +1,10 @@
+package lab.enumeration;
+
+public class UserStatusTest {
+    public static void main(String[] args) {
+        System.out.println(UserStatus.PENDING);
+        System.out.println(UserStatus.ACTIVE);
+        System.out.println(UserStatus.INACTIVE);
+        System.out.println(UserStatus.DELETED);
+    }
+}
